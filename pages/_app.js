2edffce727dc850ecafbc6b7b9bec8a87179286b
hardulous <bishtaman120@gmail.com>
2023-11-20 22:25:48 +0530

    d

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import "@/styles/globals.css"; // Here importing globals.css in _app.js will add it to every page
 import { ThemeProvider } from "styled-components";
+import "@/styles/layout.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 // Creating a theme to be used by styled-components
 const theme = {
@@ -9,9 +12,17 @@ const theme = {
 };
 
 export default function App({ Component, pageProps }) {
+  
+  // Now use the getLayout function if it's defined on the page
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />);
+  }
+
   return (
     <ThemeProvider theme={theme}>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </ThemeProvider>
   );
 }
--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import React from 'react'
 
 const About = () => {
@@ -7,3 +8,13 @@ const About = () => {
 }
 
 export default About
+
+// Here this page prop is About component itself that is JSX returned by About component and now About component have its own layout different from one defined in _app.js
+About.getLayout = function PageComponent(page){
+   return (
+    <>
+     {page}
+     <Footer/> 
+    </>
+   )
+}
